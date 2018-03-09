a704b4bb0af501dfc8d755bb7ade24c6879481e3
ankita <ankita.sharma7@cognizant.com>
2018-03-09 08:57:21 +0000

    added queries.properties

--- src/main/java/com/ing/springboot/SpringBootRestApiApp.java
+++ src/main/java/com/ing/springboot/SpringBootRestApiApp.java
@@ -3,10 +3,12 @@ package com.ing.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication(scanBasePackages={"com.ing.springboot"})
 @EnableAutoConfiguration
+@PropertySource("classpath:queries.properties")
 public class SpringBootRestApiApp {
 
 	public static void main(String[] args) {
