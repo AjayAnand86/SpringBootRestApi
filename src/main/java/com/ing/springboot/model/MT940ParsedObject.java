221a82a39ba885cae97817bd7183f2af4528fe38
ankita <ankita.sharma7@cognizant.com>
2018-04-11 14:49:26 +0000

    class to be used for api

---
+++ src/main/java/com/ing/springboot/model/MT940ParsedObject.java
@@ -0,0 +1,73 @@
+package com.ing.springboot.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MT940ParsedObject {
+	private String sender;
+	private String receiver;
+	private String openingAmount;
+	private String closingAmount;
+	private String currency;
+	private List<TransactionDetails> transactionDetails;
+
+	public MT940ParsedObject() {
+		transactionDetails = new ArrayList<TransactionDetails>();
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+	public String getOpeningAmount() {
+		return openingAmount;
+	}
+
+	public void setOpeningAmount(String openingAmount) {
+		this.openingAmount = openingAmount;
+	}
+
+	public String getClosingAmount() {
+		return closingAmount;
+	}
+
+	public void setClosingAmount(String closingAmount) {
+		this.closingAmount = closingAmount;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+	public List<TransactionDetails> getTransactionDetails() {
+		return transactionDetails;
+	}
+
+	public void setTransactionDetails(List<TransactionDetails> transactionDetails) {
+		this.transactionDetails = transactionDetails;
+	}
+
+	@Override
+	public String toString() {
+		return "MT940ParsedObject [sender=" + sender + ", receiver=" + receiver + ", openingAmount=" + openingAmount
+				+ ", closingAmount=" + closingAmount + ", currency=" + currency + ", transactionDetails="
+				+ transactionDetails + "]";
+	}
+
+}
